9c2f63132a5a90eff0d58ebbd2bd3bc392b8d4f7
Abhishek Holla <93963745+AbHolla@users.noreply.github.com>
2025-09-29 20:15:23 +0530

    Time: 27 ms (50.27%) | Memory: 61.7 MB (68.80%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+    s.sort((a,b) =>(a-b));
+    g.sort((a,b) =>(a-b));
+
+    let i=0;
+    let j=0;
+    while(j<s.length){
+        if(s[j]>=g[i]){
+            i++;
+        }
+        j++;
+    }
+    return i;
+
+};
