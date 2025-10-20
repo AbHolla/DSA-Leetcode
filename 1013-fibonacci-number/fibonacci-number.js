b8c3bc2ec9d6a8c33cd6b57556edb929b654f423
Abhishek Holla <93963745+AbHolla@users.noreply.github.com>
2025-10-20 18:13:38 +0530

    Time: 49 ms (63.21%) | Memory: 54 MB (37.49%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -6,8 +6,15 @@ var fib = function(n) {
     // if(n==0){
     //     return 0;
     // }
-    if(n<=1){
-        return n;
+    // if(n<=1){
+    //     return n;
+    // }
+    // return fib(n-1)+fib(n-2);
+
+
+    let dp=[0,1];
+    for(let i=2;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2];
     }
-    return fib(n-1)+fib(n-2);
+    return dp[n];
 };
