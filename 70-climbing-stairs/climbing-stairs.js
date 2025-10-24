c8d148f346e12a8948bc797c2fafb1610eaa0d7a
Abhishek Holla <93963745+AbHolla@users.noreply.github.com>
2025-10-24 17:50:17 +0530

    Time: 0 ms (100.00%) | Memory: 54.4 MB (13.58%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -2,20 +2,20 @@
  * @param {number} n
  * @return {number}
  */
+let dp={};
 var climbStairs = function(n) {
-// let dp={};
-//     if(n<=2){
-//         return n;
-//     }
-//     if(!dp[n]){
-//         dp[n]=climbStairs(n-1)+climbStairs(n-2);
-//     }
-//     return dp[n];
-
-    let dp=[0,1,2];
-    for(let i=3;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
+    if(n<=2){
+        return n;
+    }
+    if(!dp[n]){
+        dp[n]=climbStairs(n-1)+climbStairs(n-2);
     }
     return dp[n];
+
+    // let dp=[0,1,2];
+    // for(let i=3;i<=n;i++){
+    //     dp[i]=dp[i-1]+dp[i-2];
+    // }
+    // return dp[n];
 
 };
